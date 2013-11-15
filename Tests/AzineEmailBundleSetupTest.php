67cf3ce9354dcbf614107d5506073a73588a4c91
dominik <dominik.businger@azine-it.ch>
2013-11-15 09:52:05 +0100

    test if magic-quotes is turned off

---
+++ Tests/AzineEmailBundleSetupTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace Azine\EmailBundle\Tests;
+
+/**
+ * @author d.businger
+ */
+class AzineEmailBundleSetupTest extends \PHPUnit_Framework_TestCase{
+
+	public function testMagicQuotes(){
+		$this->assertEquals(0, get_magic_quotes_gpc(), "magic_quotes_gpc should be turned off in php.ini");
+	}
+}
