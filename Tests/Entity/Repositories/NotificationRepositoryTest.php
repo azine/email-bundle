69352f84b23a65d7b986233d850406ad9d891d76
Dominik Businger <github@azine-it.ch>
2016-03-18 23:46:57 +0100

    add empty test class for notification repo.

---
+++ Tests/Entity/Repositories/NotificationRepositoryTest.php
@@ -0,0 +1,9 @@
+<?php
+namespace Azine\EmailBundle\Tests\Entity\Repositories;
+
+class NotificationRepositoryTest extends \PHPUnit_Framework_TestCase
+{
+    public function testNothing(){
+        // without database and test-data, there's no use in testing this class.
+    }
+}
