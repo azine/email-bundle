7e65daa9b21e3136484141a419e9ff14a2a9356d
azine.me <github@azine-it.ch>
2026-08-11 22:29:10 +0200

    Modernize web-view retention command for Symfony 7

--- Command/RemoveOldWebViewEmailsCommand.php
+++ Command/RemoveOldWebViewEmailsCommand.php
@@ -1,91 +1,75 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Azine\EmailBundle\Command;
 
+use Azine\EmailBundle\Entity\SentEmail;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-/**
- * Remove emails stored for web-view that are older than the configured time.
- *
- * @author dominik
- */
+#[AsCommand(
+    name: 'emails:remove-old-web-view-emails',
+    description: 'Remove stored email web views older than the configured retention period.',
+)]
 class RemoveOldWebViewEmailsCommand extends Command
 {
-    /** @var ContainerInterface|null */
-    private $container;
-
-    public function setContainer(?ContainerInterface $container = null): ?ContainerInterface
-    {
-        $previous = $this->container;
-        $this->container = $container;
-
-        return $previous;
+    public function __construct(
+        private readonly ManagerRegistry $managerRegistry,
+        private readonly int $retentionDays,
+    ) {
+        parent::__construct();
     }
 
-    protected function getContainer(): ContainerInterface
-    {
-        if (null === $this->container) {
-            throw new \LogicException('Container has not been set.');
-        }
-
-        return $this->container;
-    }
-
-    /**
-     * (non-PHPdoc).
-     *
-     * @see Symfony\Component\Console\Command.Command::configure()
-     */
     protected function configure(): void
     {
-        $this->setName('emails:remove-old-web-view-emails')
-                ->setDescription('Remove all "SentEmail" from the database that are older than the configured time.')
-                ->setDefinition(array(new InputArgument('keep', InputArgument::OPTIONAL, 'Remove all SentEmails older than "keep" days => also see azine_email_web_view_retention_time ')))
-                ->setHelp(<<<EOF
-The <info>emails:remove-old-web-view-emails</info> command deletes all SentEmail entities from the database
-that are older than the number of days specified in the command-line parameter "keep" or configured in the
-parameter "azine_email_web_view_retention" in your config.yml.
-EOF
-                )
-        ;
+        $this
+            ->addArgument(
+                'keep',
+                InputArgument::OPTIONAL,
+                'Remove stored emails older than this number of days.',
+            )
+            ->setHelp(<<<'HELP'
+The <info>emails:remove-old-web-view-emails</info> command deletes SentEmail entities older than
+"keep" days. When the argument is omitted, azine_email.web_view_retention is used.
+HELP
+            );
     }
 
-    /**
-     * (non-PHPdoc).
-     *
-     * @see Symfony\Component\Console\Command.Command::execute()
-     */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        // get the number of days from the command-line-input
-        $days = $input->getArgument('keep');
+        $argument = $input->getArgument('keep');
+        $days = is_numeric($argument) ? (int) $argument : $this->retentionDays;
 
-        // or if it is not given, from the config.yml
-        if (!is_numeric($days)) {
-            $days = $this->getContainer()->getParameter('azine_email_web_view_retention');
-            $output->writeln("using the parameter from the configuration => '$days' days.");
+        if ($days < 1) {
+            $output->writeln('<error>The web-view retention period must be at least one day.</error>');
+
+            return Command::INVALID;
         }
 
-        if (null === $days) {
-            $output->writeln('either the commandline parameter "keep" or the "azine_email_web_view_retention" in your config.yml or the default-config has to be defined.');
-
-            return Command::SUCCESS;
+        if (!is_numeric($argument)) {
+            $output->writeln(sprintf('Using the configured retention period: %d days.', $days));
         }
 
-        // delete all SentEmails older than $date from the database
-        $date = new \DateTime("$days days ago");
-        $sentEmails = $this->getContainer()->get('doctrine')->getManager()->createQueryBuilder()
-            ->delete('AzineEmailBundle:SentEmail', 's')
+        $cutoff = new \DateTimeImmutable(sprintf('-%d days', $days));
+        $deleted = $this->managerRegistry
+            ->getManager()
+            ->createQueryBuilder()
+            ->delete(SentEmail::class, 's')
             ->where('s.sent < :sent')
-            ->setParameter('sent', $date);
-        $q = $sentEmails->getQuery();
-        $result = $q->execute();
+            ->setParameter('sent', $cutoff)
+            ->getQuery()
+            ->execute();
 
-        $output->writeln($result.' SentEmails have been deleted that were older than '.$date->format('Y-m-d H:i:s'));
+        $output->writeln(sprintf(
+            '%d SentEmails older than %s were deleted.',
+            (int) $deleted,
+            $cutoff->format('Y-m-d H:i:s'),
+        ));
 
         return Command::SUCCESS;
     }
