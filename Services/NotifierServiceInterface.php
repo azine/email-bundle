03268cbbb7beda721fc0390dec1005e4f27fa784
azine.me <github@azine-it.ch>
2026-08-11 22:31:41 +0200

    Preserve notifier extension compatibility during Symfony upgrade

--- Services/NotifierServiceInterface.php
+++ Services/NotifierServiceInterface.php
@@ -6,7 +6,7 @@ namespace Azine\EmailBundle\Services;
 
 interface NotifierServiceInterface
 {
-    public function sendNotifications(array &$failedAddresses): int;
+    public function sendNotifications(array &$failedAddresses);
 
-    public function sendNewsletter(array &$failedAddresses): int;
+    public function sendNewsletter(array &$failedAddresses);
 }
